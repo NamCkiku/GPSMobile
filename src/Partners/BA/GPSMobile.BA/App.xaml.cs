bb799d06a9dacbeb744f3978500121481848a52d
trần hoàng nam <namth2@binhanh.vn>
2022-08-10 14:06:41 +0700

    add font

--- src/Partners/BA/GPSMobile.BA/App.xaml.cs
+++ src/Partners/BA/GPSMobile.BA/App.xaml.cs
@@ -1,11 +1,15 @@
-﻿namespace GPSMobile.BA
+﻿using GPSMobile.BA.Resources.Themes;
+using GPSMobile.Core.Resources.Styles;
+
+namespace GPSMobile.BA
 {
     public partial class App : Application
     {
         public App()
         {
+            Resources.MergedDictionaries.Add(new LightTheme());
+            Resources.MergedDictionaries.Add(new Styles());
             InitializeComponent();
-
             MainPage = new MainPage();
         }
     }
--- src/Partners/BA/GPSMobile.BA/MauiProgram.cs
+++ src/Partners/BA/GPSMobile.BA/MauiProgram.cs
@@ -9,8 +9,8 @@
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Roboto-Regular.ttf", "RobotoRegular");
+                    fonts.AddFont("Roboto-Bold.ttf", "RobotoBold");
                 });
 
             return builder.Build();
