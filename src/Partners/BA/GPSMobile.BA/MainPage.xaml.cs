cc476f328b86fcaa9686a4414b90f9f5a21bf0a2
trần hoàng nam <namth2@binhanh.vn>
2022-08-10 13:18:19 +0700

    first commit

---
+++ src/Partners/BA/GPSMobile.BA/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace GPSMobile.BA
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked 312312 {count} time";
+            else
+                CounterBtn.Text = $"Clicked áđâsdá {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
